572a2c798e18471aa4d1dde3567f8fc2bd4b87ce
laujamie <36782341+laujamie@users.noreply.github.com>
2024-03-11 18:17:09 -0400

    Add session provider

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,6 +1,8 @@
 "use client";
 import type { ReactNode } from "react";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
+import { SessionProvider } from "convex-helpers/react/sessions.js";
+import { useLocalStorage } from "usehooks-ts";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
@@ -9,5 +11,14 @@ export default function ConvexClientProvider({
 }: {
   children: ReactNode;
 }) {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ConvexProvider client={convex}>
+      <SessionProvider
+        useStorage={useLocalStorage}
+        storageKey="music-queuer-session-id"
+      >
+        {children}
+      </SessionProvider>
+    </ConvexProvider>
+  );
 }
