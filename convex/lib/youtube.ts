69dd9c90bed68c927d85c72dcde5303ba9e464d6
laujamie <36782341+laujamie@users.noreply.github.com>
2024-03-14 00:01:51 -0400

    Add initial Convex action for YouTube search API

---
+++ convex/lib/youtube.ts
@@ -0,0 +1,74 @@
+type ThumbnailObj = {
+  height: number;
+  url: string;
+  width: number;
+};
+
+type IdObj = {
+  kind: string;
+  videoId: string;
+};
+
+type SnippetObj = {
+  channelId: string;
+  channelTitle: string;
+  description: string;
+  liveBroadcastContent: string;
+  publishTime: string;
+  publishedAt: string;
+  thumbnails: {
+    default: ThumbnailObj;
+    high: ThumbnailObj;
+    medium: ThumbnailObj;
+  };
+  title: string;
+};
+
+type ListResponse = {
+  kind: string;
+  etag: string;
+  nextPageToken: string;
+  prevPageToken: string;
+  regionCode: string;
+  pageInfo: {
+    totalResults: number;
+    resultsPerPage: number;
+  };
+  items: object[];
+  id?: IdObj;
+  snippet?: SnippetObj;
+};
+
+const YT_DATA_API_BASE_URL = "https://www.googleapis.com/youtube/v3";
+
+/**
+ * This function calls the list operation from the YouTube Data API. For more details on the different options
+ * that are available, see here: https://developers.google.com/youtube/v3/docs/search/list
+ * @param query - Query to search for
+ * @param parts - Parts to include from the search result (should be set to "snippet")
+ * @param types - Types to include ("video", "channel", "playlist")
+ * @param maxResults - Maximum number of results to return
+ * @param pageToken - Token to retrieve a specific page of results
+ * @returns
+ */
+export async function list(
+  query: string,
+  parts: string[] = ["snippet"],
+  types: string[] = ["video"],
+  maxResults: number = 10,
+  pageToken?: string
+): Promise<ListResponse> {
+  const reqUrl = new URL(`${YT_DATA_API_BASE_URL}/search`);
+  parts.forEach((part) => reqUrl.searchParams.append("part", part));
+  types.forEach((type) => reqUrl.searchParams.append("type", type));
+  reqUrl.searchParams.append("q", query);
+  reqUrl.searchParams.append("key", process.env.YOUTUBE_DATA_API_KEY!);
+  reqUrl.searchParams.append("maxResults", String(maxResults));
+  if (pageToken != null) {
+    reqUrl.searchParams.append("pageToken", pageToken);
+  }
+  const res = await fetch(reqUrl, {
+    method: "GET",
+  });
+  return res.json() as Promise<ListResponse>;
+}
---
+++ convex/youtube.ts
@@ -0,0 +1,10 @@
+import { v } from "convex/values";
+import { action } from "./_generated/server";
+import { list } from "./lib/youtube";
+
+export const search = action({
+  args: { query: v.string() },
+  handler: async (_, args) => {
+    return await list(args.query);
+  },
+});
