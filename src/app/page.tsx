d66a17177d0321a7080552b80744cc26b0373b51
laujamie <36782341+laujamie@users.noreply.github.com>
2024-06-28 00:28:03 -0400

    add quick blurb to get started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,53 +19,68 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col gap-y-4 justify-center items-center">
-      <div className="flex gap-x-4">
-        <Input
-          value={roomCode}
-          onChange={(e) => setRoomCode(e.target.value)}
-          placeholder="ABCD"
-        />
+    <>
+      <section className="flex flex-col gap-y-4 justify-center items-center">
+        <div className="flex gap-x-4">
+          <Input
+            value={roomCode}
+            onChange={(e) => setRoomCode(e.target.value)}
+            placeholder="ABCD"
+          />
+          <Button
+            disabled={createRoomLoading || joinRoomLoading}
+            onClick={async () => {
+              setCreateRoomLoading(true);
+              try {
+                await joinRoom({ queueCode: roomCode });
+                router.push(`/${roomCode}`);
+              } catch {
+                toast.error(`Failed to join room with code ${roomCode}`);
+              } finally {
+                setCreateRoomLoading(false);
+              }
+            }}
+          >
+            {createRoomLoading && (
+              <LoaderCircleIcon className="mr-2 h-4 w-4 animate-spin" />
+            )}{" "}
+            Join Room
+          </Button>
+        </div>
         <Button
-          disabled={createRoomLoading || joinRoomLoading}
+          variant="secondary"
+          disabled={joinRoomLoading || createRoomLoading}
           onClick={async () => {
-            setCreateRoomLoading(true);
+            setJoinRoomLoading(true);
             try {
-              await joinRoom({ queueCode: roomCode });
-              router.push(`/${roomCode}`);
-            } catch {
-              toast.error(`Failed to join room with code ${roomCode}`);
+              const { code } = await createRoom();
+              router.push(`/${code}`);
+            } catch (e) {
+              toast.error("Failed to create room");
             } finally {
-              setCreateRoomLoading(false);
+              setJoinRoomLoading(false);
             }
           }}
         >
-          {createRoomLoading && (
+          {joinRoomLoading && (
             <LoaderCircleIcon className="mr-2 h-4 w-4 animate-spin" />
           )}{" "}
-          Join Room
+          Host Room
         </Button>
-      </div>
-      <Button
-        variant="secondary"
-        disabled={joinRoomLoading || createRoomLoading}
-        onClick={async () => {
-          setJoinRoomLoading(true);
-          try {
-            const { code } = await createRoom();
-            router.push(`/${code}`);
-          } catch (e) {
-            toast.error("Failed to create room");
-          } finally {
-            setJoinRoomLoading(false);
-          }
-        }}
-      >
-        {joinRoomLoading && (
-          <LoaderCircleIcon className="mr-2 h-4 w-4 animate-spin" />
-        )}{" "}
-        Host Room
-      </Button>
-    </div>
+      </section>
+      <section className="mt-16 flex flex-col gap-y-4">
+        <h2 className="text-xl font-semibold">What is this website?</h2>
+        <p>
+          The goal of this website is to let you easily play YouTube videos at
+          home with the ability to queue up videos, similar to how a karaoke
+          tablet would work. The videos play on the host, while everyone else
+          can connect to the room to suggest songs.
+        </p>
+        <p>
+          To get started, you can host a room, connect the screen to a TV, and
+          then share the link with friends!
+        </p>
+      </section>
+    </>
   );
 }
